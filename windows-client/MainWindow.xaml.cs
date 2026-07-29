16906a448d68a52869d55dfa317728176e256129
Trang <trang@example.com>
2026-07-29 14:02:29 +0700

    feat: enforce topmost window behavior every second

--- windows-client/MainWindow.xaml.cs
+++ windows-client/MainWindow.xaml.cs
@@ -116,6 +116,14 @@ namespace WindowsClient
         private int pasteIndex = 0;
 
         private bool isExpanded = false;
+        private DispatcherTimer topmostTimer;
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_SHOWWINDOW = 0x0040;
 
         public MainWindow()
         {
@@ -126,6 +134,18 @@ namespace WindowsClient
             
             _proc = HookCallback;
             _hookID = SetHook(_proc);
+
+            topmostTimer = new DispatcherTimer();
+            topmostTimer.Interval = TimeSpan.FromSeconds(1);
+            topmostTimer.Tick += (s, e) => {
+                if (this.Visibility == Visibility.Visible)
+                {
+                    IntPtr handle = new WindowInteropHelper(this).Handle;
+                    SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+                    this.Topmost = true;
+                }
+            };
+            topmostTimer.Start();
         }
 
         protected override void OnSourceInitialized(EventArgs e)
